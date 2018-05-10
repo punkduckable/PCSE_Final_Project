99d36c5fc31b4c21ff782a3bc894d8557d16f791
Robert Stephany <robert.stephany@utexas.edu>
2018-05-10 18:10:52 -0500

    Updated threshold
    I switched to the quadratic threshold (from .001/N_Mesh to .5/(N_Mesh^2)) That was developed in the optimized update branch.

--- Laplace_Solver_Parallel.c
+++ Laplace_Solver_Parallel.c
@@ -11,7 +11,7 @@
 #define Y_Max 1.
 #define PI 3.14159265358979323846
 #define N_Mesh 10
-#define Change_Threshold (.001/(double)N_Mesh)
+#define Change_Threshold (.5/(double)(N_Mesh*N_Mesh))
 const unsigned int N_Rows = N_Mesh+2;
 const unsigned int N_Cols = N_Mesh+2;
 
